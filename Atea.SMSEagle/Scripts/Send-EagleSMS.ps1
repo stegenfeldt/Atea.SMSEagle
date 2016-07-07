82c02da2abe2f9d28473475490f26d1dffa411ba
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2016-07-07 14:48:05 +0200

    Script added and adopted

---
+++ Atea.SMSEagle/Scripts/Send-EagleSMS.ps1
@@ -0,0 +1,17 @@
+﻿#Adopted from example at http://www.smseagle.eu/code-samples/
+
+param(
+	[Parameter(Mandatory=$True)] [string] $SMSEagleIP,
+	[string] $Message = "SCOM and SMS Eagle Checking In!",
+	[Parameter(Mandatory=$True)] [string] $Receiver = "123456789",
+	[string] $User = "admin",
+	[string] $Password = "password"
+)
+
+$smseagleUrl = "http://$SMSEagleIP/index.php/jsonrpc/sms"
+
+$header = @{"Content-Type"="text/plain"}
+
+$jsonBody=@{method="sms.send_sms";params=@{login=$smseagleLogin;pass=$smseaglePass;to=$Receiver;message=$Message}} | ConvertTo-Json -Compress
+
+Invoke-RestMethod -Uri $smseagleUrl -Headers $header -Body $jsonBody -Method Post
