5fe22ef884e645a33e98eb0988192c371311d5e0
Samuel Tegenfeldt <stegenfeldt@outlook.com>
2016-07-14 20:06:28 +0200

    Began working on RunAs Accounts and a proper watcher node class. Build and functions OK.

--- Atea.SMSEagle/Scripts/Send-EagleSMS.ps1
+++ Atea.SMSEagle/Scripts/Send-EagleSMS.ps1
@@ -8,6 +8,8 @@ param(
 	[string] $Password = "password"
 )
 
+$omApi = New-Object -ComObject "MOM.ScriptAPI"
+
 $smseagleUrl = "http://$SMSEagleIP/index.php/jsonrpc/sms"
 
 $header = @{"Content-Type"="text/plain"}
@@ -15,3 +17,7 @@ $header = @{"Content-Type"="text/plain"}
 $jsonBody=@{method="sms.send_sms";params=@{login=$User;pass=$Password;to=$Receiver;message=$Message;flash=1}} | ConvertTo-Json -Compress
 
 Invoke-RestMethod -Uri $smseagleUrl -Headers $header -Body $jsonBody -Method Post
+
+[string] $omEventMessage = ("URL: {0}`nHeaders:`n{1}`nJSON Body:`n{2}" -f $smseagleUrl,$($header),$($jsonBody))
+
+$omApi.LogScriptEvent("Send-EagleSMS",6176,0,$omEventMessage)
